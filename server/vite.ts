eb68f53ee0628b7df2c0ac6561e0f4478b52cacb
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Air.local>
2025-03-15 12:25:42 -0500

    vite fix

--- server/vite.ts
+++ server/vite.ts
@@ -79,9 +79,10 @@ export function serveStatic(app: Express) {
     );
   }
 
+  console.log(`Serving static files from: ${distPath}`);
+  
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
